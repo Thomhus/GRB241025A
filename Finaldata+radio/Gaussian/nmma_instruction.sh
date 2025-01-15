6aa73ec6495245b921d1a10662b7d77dce6c68db
hussenot <hussenot@>
2025-01-16 00:40:24 +0100

    Launching analysis with final EM+X+radio set, + tests only-post-05day

---
+++ Finaldata+radio/Gaussian/nmma_instruction.sh
@@ -0,0 +1,1 @@
+lightcurve-analysis --model TrPi2018 --outdir outdirGRBgauss32cores --label GRB241025A --prior ./TrPi2018GRB241025A.prior --conditional-gaussian-prior-thetaObs --conditional-gaussian-prior-N-sigma 1 --tmin 0.01 --tmax 5 --dt 0.01 --nlive 2048 --Ebv-max 0 --trigger-time 60608.06725207 --data ../GRB241025Afinal+X+radio.dat --plot --xlim 0,5 --ylim 32,18 --bestfit
